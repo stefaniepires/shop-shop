90bedba28708afce17fad72fc00693f67dbd5d5c
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-04-17 23:15:42 -0500

    Implemented Cart Toggle

---
+++ client/build/precache-manifest.d9840a70e0a75b8b197b496220ed109c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07298d3b060be84de9ebf842fdbab027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bff15c8c3d04bc37bf9",
+    "url": "/static/css/main.88131917.chunk.css"
+  },
+  {
+    "revision": "bfcf40bfb72c848d2ef3",
+    "url": "/static/js/2.02700c9b.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.02700c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bff15c8c3d04bc37bf9",
+    "url": "/static/js/main.7e1c53a0.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
